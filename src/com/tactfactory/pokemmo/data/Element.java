427d583de1971e00c0cc9c3404df66732d34e4e4
Sebastien Cosqueric <s.cosqueric@iia-laval.fr>
2017-10-10 16:23:18 +0200

    Create Element Class

---
+++ src/com/tactfactory/pokemmo/data/Element.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.tactfactory.pokemmo.data;
+
+/**
+ * @author scosqu
+ * Elements  1 Perso = 1 Element
+ * 			1 Attack = 1 Element
+ */
+public enum Element {
+Combat,
+Dragon,
+Eau,
+Electrik,
+Feu,
+Glace,
+Insecte,
+Normal,
+Plante,
+Poison,
+Psy,
+Roche,
+Sol,
+Spectre,
+Vol
+}
