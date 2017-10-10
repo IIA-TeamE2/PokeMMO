d22e87106d836adac5386aafb80dc1fe2de76eb4
Balidas Benjamin <balidas.benjamin@gmail.com>
2017-10-10 16:11:05 +0200

    UX Connection

---
+++ src/com/tactfactory/pokemmo/ui/Connection.java
@@ -0,0 +1,48 @@
+package com.tactfactory.pokemmo.ui;
+
+import java.util.Scanner;
+
+/**
+ * It's only print the connection's message.
+ * @author Balidas Benjamin
+ *
+ */
+public class Connection {
+	
+	/**
+	 * Ask the IP of the serv, and return the IP asked.
+	 * @return	An IP in a String format.
+	 */
+	public String askIP() {
+		System.out.println("Quel est l'IP du serveur ?");
+		Scanner sc = new Scanner(System.in);
+		String ip = sc.nextLine();
+		return ip;
+	}
+	
+	/**
+	 * Say good when the connection is established.
+	 */
+	public void etablished() {
+		System.out.println("Connexion établie.");
+	}
+	
+	/**
+	 * Say bad when the connection is established.
+	 */
+	public void failed() {
+		System.out.println("Connexion impossible.");
+	}
+	
+	/**
+	 * Ask the pseudo of the serv, and return the pseudo asked.
+	 * @return	A pseudo in a String format.
+	 */
+	public String getPseudo() {
+		System.out.println("Quel est votre pseudo ?");
+		Scanner sc = new Scanner(System.in);
+		String pseudo = sc.nextLine();
+		return pseudo;
+	}
+	
+}
