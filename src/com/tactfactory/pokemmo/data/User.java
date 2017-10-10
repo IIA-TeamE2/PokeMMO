752c8249d4eaf99c3df1f93d9017b2ad1c134b02
Sebastien Cosqueric <s.cosqueric@iia-laval.fr>
2017-10-10 16:09:31 +0200

    Implement user class with Comment

--- src/com/tactfactory/pokemmo/data/User.java
+++ src/com/tactfactory/pokemmo/data/User.java
@@ -1,7 +1,24 @@
 package com.tactfactory.pokemmo.data;
 
+/**
+ * 
+ * @author scosqu
+ * Class User with max 2 user in session
+ * 1 user = 1 Perso
+ */
 public class User {
 	
 	protected String pseudo;
 
+	// Getter Pseudo User
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	//Setter Pseudo User
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
 }
+
