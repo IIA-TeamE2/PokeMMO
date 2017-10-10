88e1b40613ff48a93c28f2e2ec4f17ac8a09fba2
Antoine <laufreyd.bronze@gmail.com>
2017-10-10 16:56:16 +0200

    All in one file

--- src/com/tactfactory/pokemmo/gameplay/GamePlay.java
+++ src/com/tactfactory/pokemmo/gameplay/GamePlay.java
@@ -3,6 +3,8 @@
  */
 package com.tactfactory.pokemmo.gameplay;
 
+import java.util.Random;
+
 /**
  * @author ALEPAG1
  *
@@ -24,7 +26,7 @@ public class GamePlay {
 		Boolean Alive = true;
 		
 		while(Alive()) {
-			int attack = attack(attaquant);
+			int attack = attack();
 			bool hit = checkHit();
 			
 			defenseDown(hit, attack, defenseur);
@@ -34,6 +36,26 @@ public class GamePlay {
 			turn();
 			
 		}
+	}
+	
+	private int attack() {
+		Random r = new Random();
+		return r.nextInt(10);
+	}
+	
+	private boolean checkHit() {
+		Random r = new Random();
+		random = r.nextInt(10);
+		
+		return random >= 5;
+	}
+	
+	private void defenseDown(boolean hit, int attack, Perso defenseur) {
+		defenseur.defense = hit ? defenseur.defense - attack : defenseur.defense;
+	}
+	
+	public Boolean isAlive(Perso defenseur){
+		return defenseur.defense > 0;
 	}
 	
 	private void turn() {
